738d474702196a565568fb90412a94f666307930
Poorna32 <poornachandraga44@gmail.com>
2026-03-06 10:22:15 +0100

    Learning Terrafrom.

--- 02-Resources/Instance.tf
+++ 02-Resources/Instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Main" {
   ami           = "ami-098d8b074faf7bc92"
   instance_type = "t3.medium"
+  security_groups = [sg-00b26069c926d674a]
   tags = {
     Name = "Poornachandra"
   }
