a6f2e2f227664b8518276393b17963d1783b91e8
Poorna32 <poornachandraga44@gmail.com>
2026-08-06 14:49:52 +0530

    Learning Terraform

--- 05-module/demo/instance.tf
+++ 05-module/demo/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-0220d79f3f480ecf5"
   vpc_security_group_ids = [ "sg-095e8d0efbeb2469e" ]
-  availability_zone = "Z02595892ZU4VXX7PZ67V"
+  availability_zone = [ "Z02595892ZU4VXX7PZ67V" ]
   instance_type = var.instance_type
 
   tags = {
