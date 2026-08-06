7422b52d6c391bd3701654cbf8eb1289d6331579
Poorna32 <poornachandraga44@gmail.com>
2026-08-06 14:47:35 +0530

    Learning Terraform

--- 05-module/main.tf
+++ 05-module/main.tf
@@ -1,7 +1,11 @@
 module "demo-1" {
   source = "./demo"
+  instance_type = "t3.small"
+  name = "frontend"
 }
 
 module "demo-2" {
   source = "./demo"
+  instance_type = "t2.micro"
+  name = "Backend"
 }
