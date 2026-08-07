e8fd5b9c14d5302bdab6e0d96b1aca952e82e066
Poorna32 <poornachandraga44@gmail.com>
2026-08-07 13:53:53 +0530

    Learning Terraform

--- 09-s3bucket/main.tf
+++ 09-s3bucket/main.tf
@@ -1,9 +1,30 @@
 terraform {
   backend "s3" {
     bucket = "poorna-terraform-practice"
-    key    = "dev/state"
+    key    = "dev/state/testing"
     region = "us-east-1"
   }
 }
 
-resource "null_resource" "state1" {}
+resource "aws_instance" "test" {
+  ami           = var.ami
+  vpc_security_group_ids = var.security_group
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+  variable "ami" {
+    default = "ami-0220d79f3f480ecf5"
+  }
+
+  variable "security_group" {
+    default = [ "sg-095e8d0efbeb2469e" ]
+  }
+
+  variable "instance_type" {
+    default = "t3.micro"
+  }
